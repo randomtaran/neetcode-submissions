fe2383c2242187509f7aa1d74dc7950c383bfbf0
Taranjit Singh <79708471+randomtaran@users.noreply.github.com>
2026-05-11 22:01:00 +0530

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices) {
+        let maxProfit = 0;
+        let i=0;
+        let j=prices.length-1;
+        let minLeft = [prices[0]]
+        for(let i=1; i<prices.length ; i++) {
+            minLeft.push(Math.min(minLeft[i-1], prices[i]));
+        }
+        console.log('minLeft: ', minLeft);
+        // return maxProfit;
+
+        for(let i = 0; i<prices.length ; i++){
+            if(prices[i] - minLeft[i] > maxProfit) {
+                maxProfit = prices[i] - minLeft[i];
+            }
+        }
+        return maxProfit;
+    }
+}
