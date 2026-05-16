475f6d835e4f98c12cb5bceac1a5cd0fdfd3d7c1
Taranjit Singh <79708471+randomtaran@users.noreply.github.com>
2026-05-16 23:48:44 +0530

    Add: longest-repeating-substring-with-replacement - submission-3

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-3.js
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {number} k
+     * @return {number}
+     */
+    characterReplacement(s, k) {
+        let hmap = {};
+        let maxL = 0;
+        let maxFreq = 0;
+        let l = 0;
+        for(let r = 0 ; r<s.length ; r++) {
+            hmap[s[r]] = ( hmap[s[r]] ?? 0 ) + 1;
+
+            maxFreq = Math.max(maxFreq, hmap[s[r]])
+
+            while((r-l+1) - maxFreq > k){
+                hmap[s[l]]--;
+                l++;
+            }
+            maxL = Math.max(maxL, r-l+1);
+        }
+        return maxL;
+    }
+}
