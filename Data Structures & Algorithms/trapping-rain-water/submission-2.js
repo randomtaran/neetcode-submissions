d1a9930a54fa6575fdcdefa2821d07cf082a751b
Taranjit Singh <79708471+randomtaran@users.noreply.github.com>
2026-07-22 16:54:11 +0530

    Add: trapping-rain-water - submission-2

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-2.js
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {number[]} height
+     * @return {number}
+     */
+    trap(height) {
+        let area = 0;
+        let maxLeft = [0];
+        let maxRight = [0];
+        let j = height.length - 1;
+        for(let i=1 ; i<height.length ; i++ && j--) {
+            maxLeft.push(Math.max(maxLeft[i-1], height[i-1]));
+            maxRight.push(Math.max(maxRight[i-1], height[j]));
+        }
+        
+        maxRight.reverse();
+        
+        for(let i = 0;i<maxRight.length ; i++) {
+            if(Math.min(maxLeft[i], maxRight[i]) - height[i] >=0){
+                area+=Math.min(maxLeft[i], maxRight[i]) - height[i];
+            }
+        }
+        return area;
+    }
+}
